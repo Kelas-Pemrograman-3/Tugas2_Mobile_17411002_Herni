134002005f0608ee14dbdcb0affcfa2e73d74846
hernidayanti <hernydayanty101@gmail.com>
2019-10-24 13:38:03 +0700

    first commit

---
+++ app/src/main/java/com/herny/tugas2/halaman2.java
@@ -0,0 +1,30 @@
+package com.herny.tugas2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class halaman2 extends AppCompatActivity {
+    TextView txtdata;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2_2);
+
+        txtdata = (TextView) findViewById(R.id.txtdata);
+        Intent datas = getIntent();
+        String npm = datas.getStringExtra("data");
+        String nama = datas.getStringExtra("datas");
+        txtdata.setText(nama + "\n"+npm);
+
+    }
+    @Override
+    public void onBackPressed(){
+        Intent back = new Intent(halaman2.this,MainActivity.class);
+        startActivity(back);
+        finish();
+    }
+}
